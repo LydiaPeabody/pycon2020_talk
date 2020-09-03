f8f3177d3e8ed158c34cc99a9beaa8b48d5cec6b
Lydia Peabody <lydia@neighbourlytics.com>
2020-09-04 00:10:49 +1000

    final (hopefully) version of slides

---
+++ scripts.py
@@ -0,0 +1,22 @@
+def subtraction(x,y):
+    return x - y
+
+
+
+import sys
+
+def subtraction(x,y):
+    return x - y
+
+def main():
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    s = subtraction(x,y)
+    print (s)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
